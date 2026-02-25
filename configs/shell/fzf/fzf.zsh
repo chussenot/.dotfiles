7cb594b917a612ab6a93442081f8dda31bdf0c9f
Clement HUSSENOT-DESENONGES <clement.hussenot-desenonges@believe.com>
2026-02-25 12:30:16 +0100

    feat: Use fd for faster file search in CTRL+T

--- configs/shell/fzf/fzf.zsh
+++ configs/shell/fzf/fzf.zsh
@@ -5,3 +5,10 @@ if [[ ! "$PATH" == */home/chussenot/.fzf/bin* ]]; then
 fi
 
 source <(fzf --zsh)
+
+# Use fd for fzf file search (CTRL+T) if available
+if command -v fd &>/dev/null; then
+  export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+  export FZF_CTRL_T_COMMAND='fd --type f --hidden --follow --exclude .git'
+  export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git'
+fi
